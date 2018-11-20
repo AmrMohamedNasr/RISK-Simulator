dd098b08cfdf854dd786d0c9071d18135bf4eb50
Amr Mohamed Nasr <re13amr@gmail.com>
2018-11-20 13:18:21 +0200

    Game finished

---
+++ src/game/RiskGame.java
@@ -0,0 +1,80 @@
+package game;
+
+import agent.Agent;
+import agent.search.SearchAgent;
+import game.model.GameBoard;
+import game.model.Player;
+import game.model.info_capsules.EndGameInformation;
+import gui.panel.GameWindow;
+
+public class RiskGame implements Game {
+	
+	private EndGameInformation endGameInfo;
+	private static final int duration = 1000;
+	
+	public RiskGame() {
+		endGameInfo = null;
+	}
+	
+	@Override
+	public void play_game(GameBoard board, Agent player_1, Agent player_2, GameWindow window) {
+		Player turn = Player.PLAYER_1;
+		int turns = 0;
+		while (!board.isGameOver()) {
+			if (turn == Player.PLAYER_1) {
+				player_1.observe_enviroment(board);
+				board.place_unit(player_1.getAgentPlayer(), player_1.place_action());
+				window.update_graph(board);
+				try {
+					Thread.sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				board.attack_node(player_1.getAgentPlayer(), player_1.attack_action());
+				window.update_graph(board);
+				try {
+					Thread.sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				turns++;
+			} else {
+				player_2.observe_enviroment(board);
+				board.place_unit(player_2.getAgentPlayer(), player_2.place_action());
+				window.update_graph(board);
+				try {
+					Thread.sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				board.attack_node(player_2.getAgentPlayer(), player_2.attack_action());
+				window.update_graph(board);
+				try {
+					Thread.sleep(duration);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		Agent winner;
+		int exp_nodes;
+		if (board.getPlayerNodesSet(Player.PLAYER_1).size() == 0) {
+			winner = player_1;
+		} else {
+			winner = player_2;
+		}
+		if (winner instanceof SearchAgent) {
+			exp_nodes = ((SearchAgent) winner).getExpandedNodesTotalNumber();
+		} else {
+			exp_nodes = -1;
+		}
+		endGameInfo = new EndGameInformation(player_1.getAgentName(),
+				player_2.getAgentName(), winner.getAgentPlayer(), exp_nodes, turns);
+	}
+
+	@Override
+	public EndGameInformation get_game_results() {
+		return endGameInfo;
+	}
+
+}
--- src/game/model/Player.java
+++ src/game/model/Player.java
@@ -2,4 +2,12 @@ package game.model;
 
 public enum Player {
 	PLAYER_1, PLAYER_2;
+	
+	public Player reverseTurn(Player player) {
+		if (player == Player.PLAYER_1) {
+			return Player.PLAYER_2;
+		} else {
+			return Player.PLAYER_1;
+		}
+	}
 }
